a7564c46efcae4fd5fcd0875e63d42f7b1deeed8
Rob Vettor <robvet@microsoft.com>
2023-02-26 14:55:03 -0600

    adding deplyment files

---
+++ Services/Catalog.Service/Deployment/convert.sh
@@ -0,0 +1,4 @@
+
+$aiinstrumkeysecret= -n 5e8a6787-0277-48bd-9dac-f6de958d844d | base64
+$sbconnstrsecret= -n sbconnstrsecret | base64
+$catalogdbsecret= Server=tcp:aca-dem-sql.database.windows.net,1433;Initial Catalog=aca-demo-sqldb;Persist Security Info=False;User ID=micr0tunessqllogin;Password=micr0tunespass@word1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30; | base64
